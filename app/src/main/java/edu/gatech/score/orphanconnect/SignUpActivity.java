eb92a03f82dbd0c9a3ef9ee74ec72dcad0816686
Joexiaohongchen <cxhkobeee@gmail.com>
2019-09-29 22:13:27 -0400

    sprint 2 update

--- app/src/main/java/edu/gatech/score/orphanconnect/SignUpActivity.java
+++ app/src/main/java/edu/gatech/score/orphanconnect/SignUpActivity.java
@@ -1,8 +1,8 @@
-package com.example.score_coding_demo;
+package edu.gatech.score.orphanconnect;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
